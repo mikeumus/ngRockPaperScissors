edcbec212aa16c2cf724f8d9b6939879ad0c5a5f
Michael D. Mooring <mikeumus@gmail.com>
2016-09-29 14:07:53 +0000

    Layout.

--- ngRPS/src/app/app.component.ts
+++ ngRPS/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '🆖 ✊🏽Rock 📰Paper ✂Scissors';
+  loaded = false;
+  choices = ["rock", "paper", "scissors"];
+  
+  setChoice(choice) { 
+    console.log(choice);
+    if(choice === "rock"){
+      loaded = true;
+      choice = "rock";
+    }
+  }
 }
