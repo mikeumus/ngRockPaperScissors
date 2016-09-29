856bdcfbbeeb1ea8e1a86562b0b4af84f98518e4
Michael D. Mooring <mikeumus@gmail.com>
2016-09-29 14:57:02 +0000

    Button choices UX.

--- ngRPS/src/app/app.component.ts
+++ ngRPS/src/app/app.component.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
+import { Http, HTTP_PROVIDERS, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-root',
+  viewProviders: [HTTP_PROVIDERS],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = '🆖 ✊🏽Rock 📰Paper ✂Scissors';
-  loaded = false;
-  choices = ["rock", "paper", "scissors"];
+  
+  player = {
+    loaded: false,
+    choices: ["rock", "paper", "scissors"],
+    choice: ""
+  }
   
   setChoice(choice) { 
     console.log(choice);
     if(choice === "rock"){
-      loaded = true;
-      choice = "rock";
+      this.player.loaded = true;
+      this.player.choice = "rock";
+    } else if(choice === "paper"){
+      this.player.loaded = true;
+      this.player.choice = "paper";
+    } else if(choice === "scissors"){
+      this.player.loaded = true;
+      this.player.choice = "scissors";
     }
   }
 }
